3dfadbc4503df4816ce97879c6afe937cf0e1970
Quincy Iheme <qiheme@gmail.com>
2020-02-06 20:23:15 -0500

    Update server.js

--- server.js
+++ server.js
@@ -60,5 +60,5 @@ app.post("/api/reservations", function(req, res) {
 // =============================================================================
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+  console.log("App listening on PORT: " + PORT);
 });
