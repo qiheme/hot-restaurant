24a83a6ef22c2bfe5171433e7fa22d42553b828e
dchen <chendana95@gmail.com>
2020-02-06 20:23:39 -0500

    created get and post paths (#5)
    * created get and post paths
    
    * Update server.js
    
    Co-authored-by: Quincy Iheme <qiheme@gmail.com>

--- server.js
+++ server.js
@@ -3,7 +3,9 @@
 // Series of npm packages that we will use to give our server useful functionality
 // ==============================================================================
 
-var express = require("express");
+const express = require("express");
+const reservation = require('reservation.js');
+const waitlist = require('waitlist.js');
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
@@ -13,15 +15,44 @@ var express = require("express");
 // Tells node that we are creating an "express" server
 
 // Sets an initial port. We"ll use this later in our listener
+var app = express();
 var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // ================================================================================
 // ROUTER
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/tables.html"));
+});
+
+app.get("/api/reservations", function(req, res) {
+    return res.json(reservation);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+});
+
+app.post("/api/reservations", function(req, res) {
+    var newReservation = req.body;
+    reservations.push(newReservation);
+    res.json(newReservation);
+});
 
 // =============================================================================
 // LISTENER
